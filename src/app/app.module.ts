07c7c2174e57b0755aa0a98c69e7791803aa7068
Kien Phuong <nguyenphuongkien21081995@gmail.com>
2019-01-20 21:53:31 +0700

    add file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    SidebarComponent,
+    FooterComponent,
+    MainpageComponent
   ],
   imports: [
     BrowserModule,
